c56a836796ffd5a87cfb02797cb43095202f539b
Simon Schmid <ssc@feinheit.ch>
2012-06-07 21:14:00 +0200

    poll unicode -> title

--- profilingpoll/models.py
+++ profilingpoll/models.py
@@ -22,7 +22,7 @@ class Poll(TimestampMixin):
     description = models.TextField(_('description'), blank=True, null=True)
 
     def __unicode__(self):
-        return truncatechars(self.description, 50)
+        return self.title
 
     @models.permalink
     def get_absolute_url(self):
