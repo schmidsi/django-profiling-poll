39c9e356dd113a633b93a94f97187c611c1a5df2
Simon Schmid <ssc@feinheit.ch>
2012-06-26 20:17:12 +0200

    refactor firstpolldescription: make it more generic and use a template

--- profilingpoll/feincms/contents.py
+++ profilingpoll/feincms/contents.py
@@ -1,15 +1,20 @@
 from django.db import models
+from django.template.loader import render_to_string
 
 from ..models import Poll
 
 
-class FirstPollDescription(models.Model):
+class FirstPoll(models.Model):
     class Meta:
         abstract = True
 
-    def render(self, **kwargs):
+    def render(self, request, **kwargs):
         try:
-            return Poll.objects.filter(active=True)[0].description
+            #Poll.objects.filter(active=True)[0].description
+            firstpoll = Poll.objects.filter(active=True)[0]
         except IndexError:
-            return None
+            firstpoll = None
+
+        return render_to_string('content/profilingpoll/firstpoll.html',
+                {'content': self, 'firstpoll' : firstpoll} )
 
